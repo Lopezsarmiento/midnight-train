bfe7d7ea263090143e975783ebe19d90cbb6a0d5
Diego Francisco Lopez Sarmiento <14796298+Lopezsarmiento@users.noreply.github.com>
2025-01-14 00:49:58 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>Next ts Starter kit</h1>
+      </div>
+    </section>
+  )
+}
